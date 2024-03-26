eb1f138e1e3469ba588e68621bafe1efceef1a5e
Eduardo Antonio <126982066+eduhantonio@users.noreply.github.com>
2024-03-26 11:57:17 -0300

    Fila.java
    - Correção no erro de ordem da fila, agora os amarelos entram na frente dos verdes
    - É necessário corrigir a ordem dos amarelos, eles devem ser sequenciais não entrarem um na frente do outro

--- Fila.java
+++ Fila.java
@@ -1,7 +1,7 @@
 public class Fila {
     No inicio;
     
-    public boolean listaVazia(){ // Verificar se a Lista está vazia
+    public boolean listaVazia() { // Verificar se a Lista está vazia
         return inicio == null;
     }
     
@@ -9,45 +9,67 @@ public class Fila {
         if (paciente.corDoCartao.equals("Verde")){
             pacienteVerde(paciente);
         } else {
-            pacienteAmarelo(paciente);
+          pacienteAmarelo(paciente);
         }
     }
+ 
+//---------------------------------------------------------------------------------------------------//  
     
     public void pacienteVerde(Paciente paciente) { // Sempre vai adicionar o paciente no fim da fila
         No novoPaciente = new No(paciente);
         No auxiliar = inicio;
         
-        if (vazia()){
+        if (listaVazia()){ 
             novoPaciente.proximo = inicio;
             inicio = novoPaciente;
         } else {
             while (auxiliar.proximo != null) {
                 auxiliar = auxiliar.proximo;
             }
-            auxiliar.proximo = novo;   
+            auxiliar.proximo = novoPaciente;   
         }
     }
+  
+  //---------------------------------------------------------------------------------------------------//  
     
     public void pacienteAmarelo(Paciente paciente){
-        if (vazia()){ // Se a fila estiver vazia, o Amarelo entra como sendo o primeiro
-            pacienteVerde();
-        } else {
-            if (inicio.paciente.corDoCartao.equals("Amarelo")) { // Tem amarelo na Fila
-                pacienteVerde();
+        No novoPaciente = new No(paciente);
+        No auxiliar = inicio;
+        
+        if (listaVazia()) {  // Se a fila estiver vazia, o Amarelo entra como sendo o primeiro
+            novoPaciente.proximo = inicio;
+            inicio = novoPaciente;
+        } else {  // Se a fila NÃO estiver vazia haverá o seguintes cenários
+        
+            // Já existe (pelo menos) um Amarelo na Fila, devo procurar o último Amarelo
+            if (inicio.paciente.corDoCartao.equals("Amarelo")){
+                novoPaciente.proximo = inicio;
+                inicio= novoPaciente;
+                while (auxiliar.paciente.corDoCartao.equals("Amarelo")) {
+                    auxiliar = auxiliar.proximo;
+                } novoPaciente.proximo = auxiliar.proximo; 
             }
             
-            else { // Tem Verde e Amarelo
-                
+            // Não existem Amarelos na Fila, então devo procurar o primeiro Verde
+            if (inicio.paciente.corDoCartao.equals("Verde")) {
+                novoPaciente.proximo = inicio;
+                inicio = novoPaciente;
+                while (auxiliar.proximo != null) {
+                    auxiliar = auxiliar.proximo;
+                } novoPaciente = inicio;
             }
         }
     }
+
+  //---------------------------------------------------------------------------------------------------//  
     
     public void imprimirDados() {
         if (!listaVazia()) {
             No auxiliar = inicio;
             while (auxiliar != null) {
-                System.out.print(auxiliar);
+                System.out.print(auxiliar.paciente);
                 auxiliar = auxiliar.proximo;
+                System.out.println(" ");
             }
         } else {
             System.out.println("Fila Vazia");
