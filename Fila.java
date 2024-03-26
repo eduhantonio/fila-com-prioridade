f18be51e2e8c0455819e2251ae1d7a072d434bdc
Eduardo Antonio <126982066+eduhantonio@users.noreply.github.com>
2024-03-26 16:12:30 -0300

    Fila.java
    Filha da puta

--- Fila.java
+++ Fila.java
@@ -1,7 +1,8 @@
 public class Fila {
     No inicio;
     
-    public boolean listaVazia() { // Verificar se a Lista está vazia
+    // Verificar se a Lista está vazia
+    public boolean listaVazia() {
         return inicio == null;
     }
     
@@ -15,7 +16,8 @@ public class Fila {
  
 //---------------------------------------------------------------------------------------------------//  
     
-    public void pacienteVerde(Paciente paciente) { // Sempre vai adicionar o paciente no fim da fila
+    // Sempre vai adicionar o paciente no fim da fila
+    public void pacienteVerde(Paciente paciente) { 
         No novoPaciente = new No(paciente);
         No auxiliar = inicio;
         
@@ -35,25 +37,30 @@ public class Fila {
     public void pacienteAmarelo(Paciente paciente){
         No novoPaciente = new No(paciente);
         No auxiliar = inicio;
-        
-        if (listaVazia()) {  // Se a fila estiver vazia, o Amarelo entra como sendo o primeiro
+     
+        // Se a fila estiver vazia, o Amarelo entra como sendo o primeiro
+        if (listaVazia()) { 
             novoPaciente.proximo = inicio;
             inicio = novoPaciente;
-        } else {  // Se a fila NÃO estiver vazia haverá o seguintes cenários
+        } 
         
-            // Já existe (pelo menos) um Amarelo na Fila, devo procurar o último Amarelo
-            if (inicio.paciente.corDoCartao.equals("Amarelo")){
-                novoPaciente.proximo = inicio;
-                inicio= novoPaciente;
-                while (auxiliar.paciente.corDoCartao.equals("Amarelo")) {
-                    auxiliar = auxiliar.proximo;
-                } novoPaciente.proximo = auxiliar.proximo; 
-            }
+        // Se a fila NÃO estiver vazia haverá o seguintes cenários
+        else {  
             
             // Não existem Amarelos na Fila, então devo procurar o primeiro Verde
             if (inicio.paciente.corDoCartao.equals("Verde")) {
                 novoPaciente.proximo = inicio;
                 inicio = novoPaciente;
+                while (auxiliar.proximo != null) {
+                    auxiliar = auxiliar.proximo;
+                } novoPaciente = inicio;
+            }
+            
+            // Já existe (pelo menos) um Amarelo na Fila, devo procurar o último Amarelo
+            else {
+                novoPaciente = inicio.proximo;
+                inicio = novoPaciente;
+            
                 while (auxiliar.proximo != null) {
                     auxiliar = auxiliar.proximo;
                 } novoPaciente = inicio;
